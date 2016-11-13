8c2f2253205b916dfe18ebc6eef587d455942ef5
Enda Lee <enda.lee@ittdublin.ie>
2016-11-13 13:08:24 +0000

    capturing inputs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ClickMeComponent } from './click-me/click-me.component';
+import { ShowMessageComponent } from './show-message/show-message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClickMeComponent,
+    ShowMessageComponent
   ],
   imports: [
     BrowserModule,
